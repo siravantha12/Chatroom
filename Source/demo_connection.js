6b69591c66dee68b76ae71ca640ec3b679425eb2
Casey Bates <cbates10@vols.utk.edu>
2018-09-16 12:41:15 -0500

    establishes database connection for #9

--- Source/demo_connection.js
+++ Source/demo_connection.js
@@ -1,15 +1,13 @@
 var mysql = require('mysql');
 
-// Connects to instance of mysql database. Currently runs off local mysql database.
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "password"
-});
-
-con.connect(function(err) {
-	if(err) throw err;
+exports.mysqlCreateConnection = function() {
 	
-	// Upon successful connection Connected will be printed on console
-	console.log("Connected");
-});
+	// Connects to instance of mysql database. Currently runs off local mysql database.
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "password"
+	});
+
+	return con;
+}
