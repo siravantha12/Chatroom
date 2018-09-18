64de8a30bc7198826896a4b477d481800b37bef2
Casey Bates <cbates10@vols.utk.edu>
2018-09-18 12:42:52 -0500

    server html changes

--- Source/server.js
+++ Source/server.js
@@ -3,7 +3,7 @@ var server = express();
 var path = require("path");
 
 server.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/View/index.html'));
+  res.sendFile(path.join(__dirname+'/userForm.html'));
 });
 
 server.listen(1337,'0.0.0.0');
