14d1d9951be3898e2bd80402b72db133fc697eed
Henry Quach <HQi2015s@gmail.com>
2018-09-18 21:20:04 -0400

    Starting View Engine,deciding if this way is better then manual comm. #6

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var server = express();
 var path = require("path");
+var ejs = require("ejs");
+
+server.set('views',path.join(__dirname+'/../assets/view'))
+server.engine('html',ejs.renderFile);
+server.set('view engine','html');
 
 server.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/userForm.html'));
+  res.render('index');
 });
+
+// server.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/userForm.html'));
+// });
 
 server.listen(1337,'0.0.0.0');
