5beff14e4013bbc42707c6d983c1d3b11e5c1855
Henry Quach <HQi2015s@gmail.com>
2018-09-18 22:07:56 -0400

    Trying to Parse info to new Pageusing engine #6

--- server/server.js
+++ server/server.js
@@ -2,13 +2,24 @@ var express = require('express');
 var server = express();
 var path = require("path");
 var ejs = require("ejs");
+var bodyParser = require('body-parser');
 
 server.set('views',path.join(__dirname+'/../assets/view'))
 server.engine('html',ejs.renderFile);
 server.set('view engine','html');
 
 server.get('/',function(req,res){
-  res.render('index');
+  res.render('simpleChat');
+});
+
+server.use(bodyParser.urlencoded({ extended:true}));
+
+server.post('/',function(req,res){
+  var parsedInfo = {};
+  parsedInfo.chatText = req.body.ChatText;
+  console.log("User said: "+ parsedInfo.chatText + "\n");
+  //var chatbox = parsedInfo.chatText;
+  //res.render('simpleChat',{chatbox:parsedInfo.chatText})
 });
 
 // server.get('/',function(req,res){
