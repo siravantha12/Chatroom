d39927909461699eaae6b817a3e876e1277d6c3f
Henry Quach <HQi2015s@gmail.com>
2018-09-25 14:40:03 -0400

    #24 Testing how Cookies work, short simple cookie example

--- server/testingCookies.js
+++ server/testingCookies.js
@@ -1,29 +1,14 @@
 var express = require('express');
 var server = express();
-var path = require("path");
-var ejs = require("ejs");
-var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-server.set('views',path.join(__dirname+'/../assets/view'))
-server.engine('html',ejs.renderFile);
-server.set('view engine','html');
+server.use(cookieParser());
 
-server.get('/',function(req,res){
-  res.render('simpleChat');
-});
-
-server.use(bodyParser.urlencoded({ extended:true}));
-
-server.post('/',function(req,res){
-  var parsedInfo = {};
-  parsedInfo.chatText = req.body.ChatText;
-  console.log("User said: "+ parsedInfo.chatText + "\n");
-  //var chatbox = parsedInfo.chatText;
-  //res.render('simpleChat',{chatbox:parsedInfo.chatText})
-});
-
-// server.get('/',function(req,res){
-//   res.sendFile(path.join(__dirname+'/userForm.html'));
-// });
+server.get('/', function(req, res){
+    res.cookie('name', 'express').send('cookie set'); //Sets name = express cookie
+    res.cookie(name, 'value', {expire: 360000 + Date.now()}); //Set expiration date (will most likley put a week before testing)
+    res.clearCookie(name); //Clear cookie with name 'name'
+ });
+ 
 
 server.listen(1337,'0.0.0.0');
