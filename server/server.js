c5d54a421b84d1829105ae8f1b2d02bb252cff94
Henry Quach <HQi2015s@gmail.com>
2018-10-08 19:49:15 -0400

    Laying out template for a view engine ejs html base to work #6

--- server/server.js
+++ server/server.js
@@ -7,23 +7,14 @@ var bodyParser = require('body-parser');
 server.set('views',path.join(__dirname+'/../assets/view'))
 server.engine('html',ejs.renderFile);
 server.set('view engine','html');
-
-server.get('/',function(req,res){
-  res.render('simpleChat');
-});
-
 server.use(bodyParser.urlencoded({ extended:true}));
 
-server.post('/',function(req,res){
-  var parsedInfo = {};
-  parsedInfo.chatText = req.body.ChatText;
-  console.log("User said: "+ parsedInfo.chatText + "\n");
-  //var chatbox = parsedInfo.chatText;
-  //res.render('simpleChat',{chatbox:parsedInfo.chatText})
+server.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/../assets/view/simpleChat.html'));
 });
 
-// server.get('/',function(req,res){
-//   res.sendFile(path.join(__dirname+'/userForm.html'));
-// });
+server.post('/',function(req,res){
+  res.write()
+});
 
-server.listen(1337,'0.0.0.0');
+server.listen(3456);
